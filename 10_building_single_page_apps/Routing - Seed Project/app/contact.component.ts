0ff0377948df2c6c730a139a05a38511fed977bb
valmassoi <rvalmassoi@gmail.com>
2016-08-04 19:31:40 -0700

    finished section 10

---
+++ 10_building_single_page_apps/Routing - Seed Project/app/contact.component.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+import {Router, CanDeactivate} from 'angular2/router'
+
+@Component({
+    templateUrl: '/app/contact.component.html'
+})
+export class ContactComponent implements CanDeactivate{
+
+    constructor(private _router: Router) {
+
+    }
+    onSubmit(form){
+      console.log(form);
+      this._router.navigate(['Albums'])
+    }
+
+  routerCanDeactivate(next, previous) {
+    //if(this.form.dirty)//dirty checking
+
+    return confirm("are you sure?")
+
+    // console.log("next", next)
+    // console.log("prev", previous)
+  }
+}
